602ca9b99b8e776303ff7a304ff069b1b8363ff1
Dmitry <st1makk@gmail.com>
2023-04-13 22:52:45 +0700

    02task

---
+++ 02/Program.cs
@@ -0,0 +1,17 @@
+﻿// на вход принимает 2 числа и выдает, 
+// какое число больше, а какое меньше.
+
+Console.WriteLine("Введите число #1: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число #2: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine($"Большее = {numberA}");
+    Console.WriteLine($"Меньшее = {numberB}");
+}
+else
+{
+    Console.WriteLine($"Большее = {numberB}");
+    Console.WriteLine($"Меньшее = {numberA}");
+}
