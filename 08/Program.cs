cb0ca9d2047e43f2473b316a0f3e4c8fbd0aa7ab
Dmitry <st1makk@gmail.com>
2023-04-13 23:58:58 +0700

    08

---
+++ 08/Program.cs
@@ -0,0 +1,13 @@
+﻿// программа, которая на вход принимает
+// число N, а на выходе показывает все четные числа от 1 до N.
+
+
+Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+Console.Write("четные числа: ");
+while (count <= number)
+{
+    Console.Write(count + " ");
+    count += 2;
+}
