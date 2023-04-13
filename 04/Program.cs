93c87e3059b560b56c12f9eba8fb6b64452ef289
Dmitry <st1makk@gmail.com>
2023-04-13 23:18:06 +0700

    04

---
+++ 04/Program.cs
@@ -0,0 +1,19 @@
+﻿// программа принимает на вход 3 числа 
+// и выдает максимальное из них.
+
+Console.WriteLine("Введите число #1: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число #2: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число #3: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (max < numberB)
+{
+    max = numberB;
+}
+if (max < numberC)
+{
+    max = numberC;
+}
+Console.WriteLine($"максимальное равно = {max}");
